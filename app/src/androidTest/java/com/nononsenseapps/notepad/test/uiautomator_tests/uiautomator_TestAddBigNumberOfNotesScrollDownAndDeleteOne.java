23ee03407554456935f9a948a333bcfe6c3f9942
Tomi Lamsa <lamsatom@gmail.com>
2016-07-04 14:10:57 +0300

    added the last test - add big number of notes etc

---
+++ app/src/androidTest/java/com/nononsenseapps/notepad/test/uiautomator_tests/uiautomator_TestAddBigNumberOfNotesScrollDownAndDeleteOne.java
@@ -0,0 +1,61 @@
+package com.nononsenseapps.notepad.test.uiautomator_tests;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiScrollable;
+import android.support.test.uiautomator.UiSelector;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.fail;
+
+public class uiautomator_TestAddBigNumberOfNotesScrollDownAndDeleteOne extends BaseTestClass{
+
+    UiDevice device;
+    private String noteName1 = "prepare food";
+    private String noteName2 = "take dogs out";
+    private String noteName3 = "water plants";
+    private String noteName4 = "sleep";
+    private String[] noteNameList = {noteName1, noteName2, noteName3, noteName4,
+            "go for a jog", "do some work", "play with the dog",
+            "work out", "do weird stuff", "read a book", "drink water",
+            "write a book", "proofread the book", "publish the book",
+            "ponder life", "build a house", "repair the house", "call contractor",
+            "write another book", "scrap the book project", "start a blog",
+            "  ", "     "};
+
+    @Before
+    public void setUp(){
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        uiautomator_helper.startApplication(device);
+    }
+
+    @Test
+    public void testAddBigNumberOfNotesScrollDownAndDeleteOne() throws Exception{
+
+        uiautomator_helper.closeDrawer(device);
+        //add notes
+        uiautomator_helper.createNotes(device, noteNameList);
+
+        //scroll to last one
+        UiScrollable recyclerView = new UiScrollable(
+                new UiSelector().resourceId("android:id/list"));
+        recyclerView.scrollTextIntoView(noteName1);
+
+        //delete the note
+        device.findObject(By.text(noteName1)).click();
+        device.findObject(By.res("com.nononsenseapps.notepad:id/menu_delete")).click();
+        device.findObject(By.res("android:id/button1")).click();
+
+        //assert that the note is no more visible
+        recyclerView = new UiScrollable(
+                new UiSelector().resourceId("android:id/list"));
+        boolean note1TitleFound = recyclerView.scrollTextIntoView(noteName1);
+
+        assertFalse("Note with name " + noteName1 + " should not be visible", note1TitleFound);
+    }
+
+}
